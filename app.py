7006dd523b390556237b8f99bd07a59d01d90c22
naimur404 <naimur404cse@gmail.com>
2024-05-04 04:32:27 +0600

    init

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, render_template, request
+from geopy.geocoders import Nominatim
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    api_key = 'be9ceaf2a3264c4da22181334240305'
+    
+    location = request.args.get('city')  # Get location from query parameter
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    if  location != '':
+       
+        url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={location}&aqi=yes"
+    else:
+        
+        url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={lat},{lon}&aqi=yes"
+    
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404, 500)
+        weather_data = response.json()
+        return jsonify(weather_data)
+    except requests.RequestException as e:
+        return jsonify({'error': f"Error fetching weather data: {e}"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
